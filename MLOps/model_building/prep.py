e178aa2d80e0a2757a96c1ebfb51d9e5b262bcda
manju30-code <manjukmoorthi@gmail.com>
2026-02-03 22:54:19 +0530

    Fix dataset path for Bank Customer Churn

--- MLOps/model_building/prep.py
+++ MLOps/model_building/prep.py
@@ -10,7 +10,7 @@ from huggingface_hub import login, HfApi
 
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
-DATASET_PATH = "hf://datasets/mkrish2025/Bank-Customer-Churn/bank-customer-churn/bank_customer_churn.csv"
+DATASET_PATH = "hf://datasets/mkrish2025/Bank-Customer-Churn/bank_customer_churn.csv"
 bank_dataset = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
